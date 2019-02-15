4416d2a17327d9e119a05dabee150a990c26b24a
dreDev <dwaynea.mclaren@gmail.com>
2019-02-15 08:17:32 -0800

    taking information from comma seperated file csv to a database

---
+++ priv/repo/seeds.exs
@@ -0,0 +1,32 @@
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     Hello.Repo.insert!(%Hello.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
+
+
+defmodule Seeds.DepartmentsAndCommunes do
+
+    @doc "Imports departments and communes from the given CSV to the database"
+    def import_from_csv(csv_path) do
+      Agent.start_link(fn -> %{departments: %HashDict{}, communes: []} end, name: __MODULE__)
+  
+      File.stream!(Path.expand(csv_path))
+      |> CSV.decode(separator: ?;, headers: true)
+      |> Stream.each(fn row ->
+        _process_csv_row(row, agent)
+      end)
+      |> Stream.run
+    end
+  
+    defp _process_csv_row(row, agent) do
+      # TODO
+    end
+  
+  end
